bde0d9d2c6bc74e750c224808b8fce70cd31cd7d
Nico Schlömer <nico.schloemer@gmail.com>
2018-07-09 18:14:13 +0200

    fix tests

--- test/test_epicycler.py
+++ test/test_epicycler.py
@@ -15,8 +15,8 @@ def test_animate():
     polygon = numpy.array(
         [[0.0, 0.0], [1.0, 0.0], [1.0, 2.0], [-0.5, 1.1], [-0.5, 2.1]]
     )
-    epicycler.animate(
-        polygon, xylim=[[-1.5, +2.0], [-1.3, 3.0]], show_axes=False, filename="out.mp4"
+    epicycler.animate_poly(
+        polygon, xylim=[-1.5, +2.0, -1.3, 3.0], show_axes=False
     )
     return
 
